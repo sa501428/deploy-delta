1fc0dccc1209789c37005c45e0f1ad0b59172106
Muhammad S Shamim <sa501428@gmail.com>
2021-10-28 23:27:21 -0500

    bug fixes for nms post processing

--- scripts/NonMaxSuppression.py
+++ scripts/NonMaxSuppression.py
@@ -1,64 +1,78 @@
 import gc
-
 import numpy as np
 
 
-def update_stripe_pick(initial_stripe, overlapping_stripe_list):
-    bounding_stripe = np.squeeze(initial_stripe) + 0
-    bounding_stripe[2] = np.min(overlapping_stripe_list[:, 2])
-    bounding_stripe[3] = np.max(overlapping_stripe_list[:, 3])
-    return bounding_stripe
+def split_boxes(boxes):
+    x1 = np.squeeze(boxes[:, 0])
+    x2 = np.squeeze(boxes[:, 1])
+    y1 = np.squeeze(boxes[:, 2])
+    y2 = np.squeeze(boxes[:, 3])
+    return x1, x2, y1, y2
 
 
-class FastNMS:
-    def __init__(self, resolution, threshold):
-        self.__resolution = resolution
-        self.__threshold = threshold
+def generate_new_pick(initial_stripe, overlapping_stripes):
+    new_stripe = np.squeeze(initial_stripe) + 0
+    new_stripe[2] = min(new_stripe[2], np.min(overlapping_stripes[:, 2]))
+    new_stripe[3] = max(new_stripe[3], np.max(overlapping_stripes[:, 3]))
+    return new_stripe
 
-    def run(self, boxes, is_stripe=False):
-        if len(boxes) == 0:
-            return []
-        boxes[:, :4] = boxes[:, :4] / self.__resolution
-        picks = []
-        x1 = boxes[:, 0]
-        x2 = boxes[:, 1]
-        y1 = boxes[:, 2]
-        y2 = boxes[:, 3]
-        area = (x2 - x1) * (y2 - y1) + 1
-        indexes = np.argsort(boxes[:, 4])
-        while len(indexes) > 0:
-            last = len(indexes) - 1
-            i = indexes[last]
-            pick = boxes[i]
-            xx1 = np.maximum(x1[i], x1[indexes[:last]])
-            yy1 = np.maximum(y1[i], y1[indexes[:last]])
-            xx2 = np.minimum(x2[i], x2[indexes[:last]])
-            yy2 = np.minimum(y2[i], y2[indexes[:last]])
-            w = np.maximum(0, xx2 - xx1)
-            h = np.maximum(0, yy2 - yy1)
-            overlap = (w * h) / area[indexes[:last]]
-            overlapping_indices = np.concatenate(([last], np.where(overlap > self.__threshold)[0]))
-            if is_stripe:
-                pick = update_stripe_pick(pick, boxes[overlapping_indices])
-            indexes = np.delete(indexes, overlapping_indices)
-            picks.append(pick)
-        picks = np.asarray(picks)
-        picks[:, :4] = picks[:, :4] * self.__resolution
-        return picks
+
+def get_overlapping_indices(pick, x1, x2, y1, y2, indexes, last):
+    xx1 = np.maximum(pick[0], x1[indexes[:last]])
+    xx2 = np.minimum(pick[1], x2[indexes[:last]])
+    yy1 = np.maximum(pick[2], y1[indexes[:last]])
+    yy2 = np.minimum(pick[3], y2[indexes[:last]])
+    perimeter = np.maximum(0, xx2 - xx1) + np.maximum(0, yy2 - yy1)
+    return np.where(perimeter > 0)[0]
+
+
+def update_stripe_merger(pick, x1, x2, y1, y2, indexes, last, boxes):
+    overlapping_indices = get_overlapping_indices(pick, x1, x2, y1, y2, indexes, last)
+    if len(overlapping_indices) > 0:
+        return generate_new_pick(pick, boxes[overlapping_indices]), overlapping_indices
+    return pick, overlapping_indices
+
+
+def nms(boxes, assimilate:bool = False):
+    if len(boxes) == 0:
+        return []
+    picks = []
+    x1, x2, y1, y2 = split_boxes(boxes)
+    indexes = np.argsort(boxes[:, 4])
+    num_boxes_left = len(indexes)
+    while num_boxes_left > 0:
+        last = num_boxes_left - 1
+        i = indexes[last]
+        pick = np.squeeze(boxes[i].copy())
+        num_overlap = 0
+        if assimilate:
+            while True:
+                pick, overlapping_indices = update_stripe_merger(pick, x1, x2, y1, y2,
+                                                                 indexes, last, boxes)
+                if num_overlap == len(overlapping_indices):
+                    break
+                num_overlap = len(overlapping_indices)
+        else:
+            overlapping_indices = get_overlapping_indices(boxes[i], x1, x2, y1, y2, indexes, last)
+        overlapping_indices = np.concatenate(([last], overlapping_indices))
+        indexes = np.delete(indexes, overlapping_indices)
+        picks.append(pick)
+        num_boxes_left = len(indexes)
+    return np.asarray(picks)
 
 
 class Handler:
-    def __init__(self, resolution, threshold=.3):
+    def __init__(self):
         self.findings = {}
-        self.resolution = resolution
-        self.nms = FastNMS(1, threshold)
 
     def do_nms_and_print_to_file(self, outfile_name, is_domain=False, is_stripe=False):
         for key in self.findings:
             temp = list(self.findings[key])
             temp = np.vstack(temp)
-            self.findings[key] = self.nms.run(temp, is_stripe=is_stripe)
-
+            if is_stripe:
+                self.findings[key] = nms(temp, assimilate=True)
+            else:
+                self.findings[key] = nms(temp)
         outfile_handler = open(outfile_name, 'w')
         outfile_handler.write("#chr1\tx1\tx2\tchr2\ty1\ty2\tname1\tscore\tstrand1\tstrand2\tcolor\n")
         for key in self.findings:
@@ -79,7 +93,6 @@ class Handler:
         return x1, x2, y1, y2, score
 
     def write_to_file(self, outfile_handler, chrom1, chrom2, results, is_domain=False):
-        results[:, :4] = results[:, :4] * self.resolution
         for k in range(np.shape(results)[0]):
             x1, x2, y1, y2, score = self.parse_bedpe_line(results, k)
             if is_domain:
@@ -94,17 +107,13 @@ class Handler:
 
 
 class MultiResHandler(Handler):
-    def __init__(self, files, output_name, threshold=.3, radii=None, is_domain=False, is_stripe=False):
-        super().__init__(1, threshold)
-        if radii is None:
-            for file in files:
-                self.fill_out_all_indices(file, 0, 0)
-        else:
-            for f in range(len(files)):
-                self.fill_out_all_indices(files[f], len(files) - f, radii[f])
+    def __init__(self, files, output_name, radii, is_domain=False, is_stripe=False):
+        super().__init__()
+        for f in range(len(files)):
+            self.fill_out_all_indices(files[f], len(files) - f, radii[f], is_stripe=is_stripe)
         self.do_nms_and_print_to_file(output_name, is_domain=is_domain, is_stripe=is_stripe)
 
-    def fill_out_all_indices(self, infile, priority, offset):
+    def fill_out_all_indices(self, infile, priority, offset, is_stripe):
         with open(infile) as f:
             for line in f:
                 if line.startswith('#'):
@@ -113,11 +122,13 @@ class MultiResHandler(Handler):
                 chrom1, x1, x2 = line_split[0], int(line_split[1]) - offset, int(line_split[2]) + offset
                 chrom2, y1, y2 = line_split[3], int(line_split[4]) - offset, int(line_split[5]) + offset
                 prediction = float(line_split[7]) + priority
-                self.add_prediction(chrom1, chrom2, x1, x2, y1, y2, prediction, offset, priority)
+                if is_stripe and x2 - x1 > y2 - y1:
+                    self.add_prediction(chrom1, chrom2, y1, y2, x1, x2, prediction, offset, priority)
+                else:
+                    self.add_prediction(chrom1, chrom2, x1, x2, y1, y2, prediction, offset, priority)
         gc.collect()
 
     def write_to_file(self, outfile_handler, chrom1, chrom2, results, is_domain=False):
-        results[:, :4] = results[:, :4] * self.resolution
         for k in range(np.shape(results)[0]):
             x1, x2, y1, y2, score = self.parse_bedpe_line(results, k)
             offset, priority = int(results[k, 5]), int(results[k, 6])
