50c2668740e7c954711f3dda8499d4b102db6b4a
Muhammad S Shamim <sa501428@gmail.com>
2021-11-16 08:48:03 -0600

    resolution bug fix

--- scripts/StrawMLTools.py
+++ scripts/StrawMLTools.py
@@ -259,7 +259,7 @@ class DeploySpears:
                 print("DONE GETTING ALL REGIONS VIA STRAW!!!", flush=True)
 
     @staticmethod
-    def find_max_in_connected_components(prediction, chrom1, x0, y0, handler):
+    def find_max_in_connected_components(prediction, chrom1, x0, y0, handler, resolution):
         labeled, n_components = label(prediction, np.ones((3, 3), dtype=np.int))
         for num in range(n_components):
             highlighted_region = (labeled == (num + 1)) * prediction
@@ -267,26 +267,26 @@ class DeploySpears:
             max_indices = np.where(highlighted_region == max_prediction)
             max_indices = [(x, y) for x, y in zip(max_indices[0], max_indices[1])]
             for maxIndex in max_indices:
-                row_start = x0 + maxIndex[0]
-                row_end = row_start + 1
-                column_start = y0 + maxIndex[1]
-                column_end = column_start + 1
+                row_start = x0 + (maxIndex[0] * resolution)
+                row_end = row_start + resolution
+                column_start = y0 + (maxIndex[1] * resolution)
+                column_end = column_start + resolution
                 if row_start < column_start:
                     handler.add_prediction(chrom1, chrom1, row_start, row_end, column_start, column_end, max_prediction)
                 else:
                     handler.add_prediction(chrom1, chrom1, column_start, column_end, row_start, row_end, max_prediction)
 
     @staticmethod
-    def find_bound_in_connected_components(prediction, chrom1, x0, y0, handler):
+    def find_bound_in_connected_components(prediction, chrom1, x0, y0, handler, resolution):
         labeled, n_components = label(prediction, np.ones((3, 3), dtype=np.int))
         for num in range(n_components):
             highlighted_region = (labeled == (num + 1)) * prediction
             max_prediction = np.max(highlighted_region)
             indices = np.where(highlighted_region > 0)
-            row_start = int(x0 + np.min(indices[0]))
-            row_end = max(int(x0 + np.max(indices[0])), row_start + 1)
-            col_start = int(y0 + np.min(indices[1]))
-            col_end = max(int(y0 + np.max(indices[1])), col_start + 1)
+            row_start = int(x0 + np.min(indices[0]) * resolution)
+            row_end = max(int(x0 + np.max(indices[0]) * resolution), row_start + resolution)
+            col_start = int(y0 + np.min(indices[1]) * resolution)
+            col_end = max(int(y0 + np.max(indices[1]) * resolution), col_start + resolution)
             if col_end - col_start > 10 * (row_end - row_start):
                 handler.add_prediction(chrom1, chrom1, row_start, row_end, col_start, col_end, max_prediction)
 
@@ -298,9 +298,11 @@ class DeploySpears:
         y1 = int(section[1][2])
         for k in range(self.__num_output_channels + 1):
             if k == 2 and self.__num_output_channels == 3:
-                self.find_bound_in_connected_components(prediction[:, :, k], chrom1, x1, y1, self.__nms_handlers[k])
+                self.find_bound_in_connected_components(prediction[:, :, k], chrom1, x1, y1,
+                                                        self.__nms_handlers[k], self.__resolution)
             else:
-                self.find_max_in_connected_components(prediction[:, :, k], chrom1, x1, y1, self.__nms_handlers[k])
+                self.find_max_in_connected_components(prediction[:, :, k], chrom1, x1, y1,
+                                                      self.__nms_handlers[k], self.__resolution)
 
     def get_threshold(self):
         return self.__threshold
